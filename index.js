3d5e41c389831f85e259933e831c566b06024111
Rounak00 <mukherjeerounak10@gmail.com>
2024-08-21 16:32:39 +0530

    alldone

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const setupSocket = require("./socket.js");
 const app=express();
 app.use(cors(
      {
-        origin: 'http://localhost:5173',
+        origin: '*',
         credentials: true,
       }
 ));
